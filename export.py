9889e542967ddb5808d85408d12b786a95a1fe10
David Schwarz <david@edgeimpulse.com>
2023-03-12 23:59:03 -0500

    fix uint8 io layers

--- export.py
+++ export.py
@@ -381,8 +381,8 @@ def export_tflite(keras_model, im, file, int8, data, nms, agnostic_nms, prefix=c
         converter.representative_dataset = lambda: representative_dataset_gen(dataset, ncalib=100)
         converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
         converter.target_spec.supported_types = []
-        converter.inference_input_type = tf.uint8  # or tf.int8
-        converter.inference_output_type = tf.uint8  # or tf.int8
+        converter.inference_input_type = tf.int8  # or tf.int8
+        converter.inference_output_type = tf.int8  # or tf.int8
         converter.experimental_new_quantizer = True
         f = str(file).replace('.pt', '-int8.tflite')
     if nms or agnostic_nms:
